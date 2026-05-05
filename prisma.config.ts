81d63de5b0073a92459c99fb0ca09e4f0cb30a7d
bhchia <48150505+bhchia@users.noreply.github.com>
2026-05-05 12:41:22 +0800

    Update prisma.config.ts
    Old scripts:
    
    import 'dotenv/config';
    import { defineConfig, env } from 'prisma/config';
    
    export default defineConfig({
      datasource: {
        url: env('DATABASE_URL'),
      },
    });

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,11 @@
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
+  schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL'),
+    // We use the Direct URL here so the Build/Migration 
+    // process can bypass the Supabase pooler.
+    url: process.env.DIRECT_DATABASE_URL,
   },
 });
